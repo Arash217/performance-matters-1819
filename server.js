464922b7c6a801cf08360277b199e7c2c0878bdd
Arash Paknezad <a.paknezad@hotmail.com>
2019-03-22 09:57:10 +0100

    added comment to server.js

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(serve(path.join(__dirname, '/public'), {
     }
 }));
 
+/* Inject files names with hashes into views by using rev-manifest.json */
 app.use(koaManifestRev({
     manifest: path.join(__dirname, 'public', 'rev-manifest.json'),
     prepend: '/'
