b5cd169af260b7438faad931740743f8b434ecb4
Arash Paknezad <a.paknezad@hotmail.com>
2019-03-28 22:23:00 +0100

    refactored offline/online modus

--- public/main.js
+++ public/main.js
@@ -6,20 +6,18 @@ if ('serviceWorker' in navigator) {
         } catch (err) {
             console.error(`Failed to register service worker: ${err}`)
         }
+
+        function handleNetworkChange() {
+            if (navigator.onLine) {
+                document.body.classList.remove('offline');
+            } else {
+                document.body.classList.add('offline');
+            }
+        }
+
+        window.addEventListener('online', handleNetworkChange);
+        window.addEventListener('offline', handleNetworkChange);
     })
 } else {
     console.log('Service worker is not supported.');
 }
-
-window.addEventListener('load', () => {
-    function handleNetworkChange() {
-        if (navigator.onLine) {
-            document.body.classList.remove('offline');
-        } else {
-            document.body.classList.add('offline');
-        }
-    }
-
-    window.addEventListener('online', handleNetworkChange);
-    window.addEventListener('offline', handleNetworkChange);
-});
